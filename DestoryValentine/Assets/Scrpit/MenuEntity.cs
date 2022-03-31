865d45e74aa49563d0574dc0bfba05443d77a7a1
UsagihimeDenka <philippe8910@gmail.com>
2022-03-31 11:00:08 +0800

    Menu實體化

--- DestoryValentine/Assets/Scrpit/MenuEntity.cs
+++ DestoryValentine/Assets/Scrpit/MenuEntity.cs
@@ -1,8 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuEntity : MonoBehaviour
 {
+   [SerializeField] private Animator animator;
+
+   [SerializeField] private bool isOpen;
+
+   public void Start()
+   {
+      animator = GetComponent<Animator>();
+   }
+
+   public void Open()
+   {
+      animator.Play("Open");
+      isOpen = true;
+   }
+
+   public void Close()
+   {
+      animator.Play("Close");
+      isOpen = false;
+   }
+
+   public void Default()
+   {
+      animator.Play("Defult");
+      isOpen = false;
+   }
+
+   public bool GetIsOpen()
+   {
+      return isOpen;
+   }
+   
    
 }
--- DestoryValentine/Assets/Scrpit/UIManager.cs
+++ DestoryValentine/Assets/Scrpit/UIManager.cs
@@ -3,27 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Project;
 using Scrpit.EventList;
+using UnityEditor;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private GameObject LosePanel;
+    [SerializeField] private MenuEntity LosePanel;
 
-    [SerializeField] private GameObject WinPanel;
+    [SerializeField] private MenuEntity WinPanel;
 
-    [SerializeField] private GameObject SettingMenu;
+    [SerializeField] private MenuEntity SettingMenu;
 
-    [SerializeField] private GameObject ShopMenu;
+    [SerializeField] private MenuEntity ShopMenu;
 
-    [SerializeField] private GameObject LevelMenu;
+    [SerializeField] private MenuEntity LevelMenu;
+
+    private List<MenuEntity> MenuEntities = new List<MenuEntity>();
     void Start()
     {
-        
+        MenuEntities.Add(LosePanel);
+        MenuEntities.Add(WinPanel);
+        MenuEntities.Add(SettingMenu);
+        MenuEntities.Add(ShopMenu);
+        MenuEntities.Add(LevelMenu);
     }
 
     public void OpenSettingMenu()
     {
-           
+        
     }
 
     public void OpenWinPanel()
@@ -48,6 +55,12 @@ public class UIManager : MonoBehaviour
 
     public void ResetAllMenu()
     {
-        
+        foreach (var VARIABLE in MenuEntities)
+        {
+            if (VARIABLE.GetIsOpen())
+            {
+                VARIABLE.Close();
+            }
+        }
     }
 }
