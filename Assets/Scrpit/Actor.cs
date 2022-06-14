0d8089a68684f33d78be58b73c8d3b33def707a1
UsagihimeDenka <philippe8910@gmail.com>
2022-06-15 00:12:28 +0800

    Import HeartBreak

--- Assets/Scrpit/Actor.cs
+++ Assets/Scrpit/Actor.cs
@@ -6,14 +6,16 @@ public class Actor : MonoBehaviour
 {
     [SerializeField] private GameObject bulletObject;
 
-    [SerializeField] private Vector3 resetPosition;
+    [SerializeField] private Vector3 resetPosition , bulletPosition;
 
-    [SerializeField] private Quaternion resetQuaternion;
+    [SerializeField] private Quaternion resetQuaternion , bulletQuaternion;
     // Start is called before the first frame update
     void Start()
     {
         resetPosition = transform.position;
         resetQuaternion = transform.rotation;
+        bulletPosition = bulletObject.transform.position;
+        bulletQuaternion = bulletObject.transform.rotation;
     }
 
     public void MoveActor(Vector3 direction)
@@ -30,6 +32,10 @@ public class Actor : MonoBehaviour
     {
         transform.position = resetPosition;
         transform.rotation = resetQuaternion;
+
+        bulletObject.transform.position = bulletPosition;
+        bulletObject.transform.rotation = resetQuaternion;
+        
         GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
         GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
---
+++ Assets/Scrpit/heart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heart : MonoBehaviour
+{
+    public GameObject fractured;
+    public float breakForce;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+            BreakTheThing();
+
+    }
+    public void BreakTheThing()
+    {
+        GameObject frac = Instantiate(fractured, transform.position, transform.rotation);
+        
+        foreach (Rigidbody rb in frac.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * breakForce;
+            rb.mass = 2.5f;
+            rb.AddForce(force);
+        }
+        Destroy(gameObject);
+    }
+
+}
